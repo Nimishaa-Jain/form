46b59e51ae1f800f579597018454fb798d39517f
Nimishaa-Jain <sonijain2oct@gmail.com>
2025-02-10 03:25:58 -0800

    Add files via upload

---
+++ nimifb/index.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+<title> fb login pg </title>
+</head>
+<body style="margin:0">
+ <div style="background-color:grey; height:1000px; width:100%;">
+<img src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" style="width:240px; margin-left: 380px;">
+
+<div style="background-color:white; height: 740px; width:450px;margin-left: 280px; border-radius: 13px; ">
+<h2 style="text-align: center; font-family: arial;"> Create a new account </h2>
+<p style="text-align:center;"> It's quick and easy </p>
+<hr>
+<input type="text" placeholder="First name" style="width: 200px; margin:10px; height: 30px;">
+<input type="text" placeholder="Surname" style="width: 200px; margin:10px; height:30px;">
+<p style="margin-left:10px; font-family: arial;"> Date Of Birth </p>
+<select style="margin-left:10px; width:120px; height:30px;">
+<option> 1 </option> 
+<option> 2  </option> 
+<option> 3 </option> 
+<option> 4 </option> 
+<option> 5 </option> 
+<option> 6 </option> 
+<option> 7 </option> 
+<option> 8 </option> 
+<option> 9 </option> 
+<option> 10 </option> 
+<option> 11 </option> 
+<option> 12 </option> 
+<option> 13 </option> 
+<option> 14 </option> 
+<option> 15 </option> 
+<option> 16 </option> 
+<option> 17 </option> 
+<option> 18 </option> 
+<option> 19 </option> 
+<option> 20 </option> 
+<option> 21 </option> 
+<option> 22 </option> 
+<option> 23 </option> 
+<option> 24 </option> 
+<option> 25 </option> 
+<option> 26 </option> 
+<option> 27 </option> 
+<option> 28 </option> 
+<option> 29 </option> 
+<option> 30 </option> 
+<option> 31 </option> 
+</select>
+
+<select style="margin-left:10px; width:120px; height:30px;">
+<option> January </option> 
+<option> February </option> 
+<option> March </option> 
+<option> April </option> 
+<option> May </option> 
+<option> June </option> 
+<option> July </option> 
+<option> August </option> 
+<option> September </option> 
+<option> October </option> 
+<option> November </option> 
+<option> December </option> 
+</select>
+
+</div>
+
+ </div>
+</body>
+</html>
